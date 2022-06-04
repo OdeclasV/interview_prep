5627512a892572a209d76924a1ba44b77b15d96e
Victor Salcedo (Aerotek US) <v-victorsa@microsoft.com>
2022-06-04 13:33:23 -0400

    new pattern: two pointers

---
+++ patterns/two_pointers/squaring_sorted_array.py
@@ -0,0 +1,30 @@
+"""
+Given a sorted array, create a new array containing squares 
+of all the numbers of the input array in the sorted order.
+
+Example:
+Input: [-2, -1, 0, 2, 3]
+Output: [0, 1, 4, 4, 9]
+"""
+
+def make_squares(arr):
+    n = len(arr)
+    # create new list in which every value is 0
+    squares = [0 for x in range(n)]
+    # used to know where biggest squared number
+    # will go in new list
+    highestSquareIdx = n - 1
+    left, right = 0, n - 1
+    while left <= right:
+        leftSquare = arr[left] * arr[left]
+        rightSquare = arr[right] * arr[right]
+        if leftSquare > rightSquare:
+            squares[highestSquareIdx] = leftSquare
+            left += 1
+        else:
+            squares[highestSquareIdx] = rightSquare
+            right -= 1
+        highestSquareIdx -= 1
+    return squares
+
+print("Squares: " + str(make_squares([-2, -1, 0, 2, 3])))
---
+++ patterns/two_pointers/test.py
@@ -0,0 +1,54 @@
+# R: string, i need longest substring of distinct chars
+# I: string, only
+# O: number, int (number of distinct chars)
+# T: 
+## what happens if empty -- output 0, or useful msg to user
+
+
+
+# example:
+## string: "aabccbb"
+## output: 3
+
+# example2:
+## string: "aabccbb"
+## output: 3, abc
+
+# approach
+## go thru each item in string
+## make sure you save to that char in temp place, hashamp
+# if that item is already in my map
+# dont add it
+## if not there, add it
+# i need to return number of items in hashmap with onl, item = value, i to return when value =1 
+# need to keep a counter, or some sort of way to count number of distinct items in hashmap to return
+
+
+
+def distinct_chars(str):
+    char_frequency = {}
+    window_start = 0
+    count_of_distinct_chars = 0
+
+    if len(str) == 0:
+        return 0
+
+    for window_end in range(len(str)):
+        right_char = str[window_end]
+        if right_char not in char_frequency:
+            char_frequency[right_char] = 0
+        char_frequency[right_char] += 1
+        while char_frequency[right_char] > 1:
+            #window_start = 
+            count_of_distinct_chars = max(count_of_distinct_chars, len(char_frequency))
+        print(char_frequency)
+
+    return count_of_distinct_chars
+
+
+# print(distinct_chars("aabccbb")) # 3
+# print(distinct_chars("abbbb"))   # 2 
+print(distinct_chars("abccde"))  # 3 abc, cde
+# print(distinct_chars(""))        # 0
+
+# example
